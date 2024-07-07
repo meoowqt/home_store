6b3e291e0f89ac65b5b05823561e062c91e988be
Vechkanova <vechkanova.p@gmail.com>
2024-07-07 10:33:53 +0400

    Создана главная страница

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    context = {"title": "Home", "content": "Главная страница магазина - Home"}
+
+    return render(request, "main/index.html")
+
+def about(request):
+    return HttpResponse("About page")
