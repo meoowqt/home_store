bda4471c7555b2ee987c9a063df5661993d18ea1
Vechkanova <vechkanova.p@gmail.com>
2024-07-07 17:26:40 +0400

    url адреса сделаны более корректно

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from main import views
 
-app_name = 'main'
+from django.urls import include, path
+from django.contrib import admin
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-]
+    path("admin/", admin.site.urls),
+    path("", include("main.urls", namespace='main'))
+    ]
