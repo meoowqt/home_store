a8210d386017d6230e1c121c5d48083ac19d88ee
Vechkanova <vechkanova.p@gmail.com>
2024-07-31 19:18:56 +0400

    Подключена админ панель

--- app/goods/models.py
+++ app/goods/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True)
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+
+
+class Meta:
+    db_table = "category"
